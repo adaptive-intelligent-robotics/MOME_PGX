ba0eb9bbc63dce162f6033001cc27276545adc36
hnj21 <hnj21@ic.ac.uk>
2023-01-11 12:12:48 +0000

    don't plot median of emitter counts and use rolling average

--- analysis.py
+++ analysis.py
@@ -17,8 +17,9 @@ from qdax.utils.plotting import (
 
 def run_analysis(dirname: str,
     experiment_names: List[str],
-    emitter_names: List[List[str]],
-    emitter_labels: List[List[str]],
+    experiment_labels: List[str],
+    emitter_names: Dict,
+    emitter_labels: Dict,
     plot_max_scores: bool=True,
 ) -> None:
 
@@ -28,46 +29,57 @@ def run_analysis(dirname: str,
     uq_metrics_list = []
     repertoires_list = []
 
+    _analysis_dir = os.path.join(dirname, "analysis/")
+    _plots_dir = os.path.join(_analysis_dir, "plots/")
+    _emitter_plots_dir = os.path.join(_plots_dir, "emitters/")
+    _median_metrics_dir = os.path.join(_analysis_dir, "median_metrics/")
+
+    os.makedirs(_analysis_dir, exist_ok=True)
+    os.makedirs(_plots_dir, exist_ok=True)
+    os.makedirs(_emitter_plots_dir, exist_ok=True)
+    os.makedirs(_median_metrics_dir, exist_ok=True)
+
     for experiment_name in experiment_names:
         experiment_metrics = get_metrics(dirname, experiment_name)
         median_metrics = experiment_metrics.median()
+        median_metrics.to_csv(f"{_median_metrics_dir}{experiment_name}_median_metrics")
         lq_metrics = experiment_metrics.quantile(q=0.25)
         uq_metrics =  experiment_metrics.quantile(q=0.75)
-        #repertoire = load_repertoire(dirname, experiment_name)
 
         metrics_list.append(experiment_metrics)
         median_metrics_list.append(median_metrics)
         lq_metrics_list.append(lq_metrics)
         uq_metrics_list.append(uq_metrics)
 
-    
+
 
     plot_max_scores_evolution(median_metrics_list, 
                             lq_metrics_list,
                             uq_metrics_list,
-                            experiment_names, 
-                            dirname
+                            experiment_labels, 
+                            _plots_dir
     )
      
     plot_hypervolumes(median_metrics_list, 
                     lq_metrics_list,
                     uq_metrics_list,
-                    experiment_names, 
-                    dirname
+                    experiment_labels, 
+                    _plots_dir
     )   
 
-    plot_emitter_counts(median_metrics_list,
+    plot_emitter_counts(metrics_list,
                     emitter_names,
                     emitter_labels,
                     experiment_names,
-                    dirname
+                    experiment_labels,
+                    _emitter_plots_dir
     )
 
     plot_coverage_scores(median_metrics_list, 
                     lq_metrics_list,
                     uq_metrics_list,
-                    experiment_names, 
-                    dirname
+                    experiment_labels, 
+                    _plots_dir
     )
 
     return
@@ -88,7 +100,7 @@ def get_metrics(dirname: str, experiment_name: str) -> pd.DataFrame:
 def plot_hypervolumes(median_metrics: List[pd.DataFrame],
     lq_metrics: List[pd.DataFrame],
     uq_metrics: List[pd.DataFrame],
-    experiment_names: List[str],
+    experiment_labels: List[str],
     save_dir: str,
 ) -> None:
 
@@ -108,7 +120,7 @@ def plot_hypervolumes(median_metrics: List[pd.DataFrame],
 
     fig, ax = plt.subplots(figsize=(18, 6), ncols=2)
 
-    for exp_num, exp_name in enumerate(experiment_names):
+    for exp_num, exp_name in enumerate(experiment_labels):
         ax[0].set_xlabel(x_label)
         ax[0].set_ylabel("MOQD Score")
         ax[0].set_title("MOQD Score evolution during training")
@@ -142,7 +154,7 @@ def plot_hypervolumes(median_metrics: List[pd.DataFrame],
 def plot_max_scores_evolution(median_metrics: List[pd.DataFrame],
     lq_metrics: List[pd.DataFrame],
     uq_metrics: List[pd.DataFrame],
-    experiment_names: List[str],
+    experiment_labels: List[str],
     save_dir: str,
 ) -> None:
 
@@ -162,7 +174,7 @@ def plot_max_scores_evolution(median_metrics: List[pd.DataFrame],
 
     fig, ax = plt.subplots(figsize=(18, 6), ncols=2)
 
-    for exp_num, exp_name in enumerate(experiment_names):
+    for exp_num, exp_name in enumerate(experiment_labels):
         ax[0].set_xlabel(x_label)
         ax[0].set_ylabel("Maximum hypervolume")
         ax[0].set_title("Maximum hypervolume evolution during training")
@@ -191,44 +203,60 @@ def plot_max_scores_evolution(median_metrics: List[pd.DataFrame],
     plt.savefig(os.path.join(save_dir, f"max_scores_evolution"))
     plt.close()
 
+def moving_average(x, w):
+    return np.convolve(x, np.ones(w), 'valid') / w    
 
-def plot_emitter_counts(median_metrics: List[pd.DataFrame],
-    emitter_names: List[List[str]],
-    emitter_labels: List[List[str]],
+def plot_emitter_counts(metrics_list: List[pd.DataFrame],
+    emitter_names: Dict,
+    emitter_labels: Dict,
     experiment_names: List[str],
+    experiment_labels: List[str],
     save_dir: str,
+    rolling_window_size: int=50,
+    num_iterations: int=4000,
 ) -> None:
 
+    # For each experiment
     for exp_num, exp_name in enumerate(experiment_names):
-        emitter_counts = []
-        for emitter in emitter_names[exp_num]:
-            emitter_count = median_metrics[exp_num][emitter]
-            emitter_counts.append(emitter_count)
-    
-        num_iterations = 4000
+        # Convert pd dfs into list of df with columns for each replication
+        emitter_counts_df_list = []
+        for emitter in emitter_names[exp_name]:
+            emitter_count_df = metrics_list[exp_num].obj.groupby(level=0).agg(list)[emitter].apply(pd.Series)
+            emitter_counts_df_list.append(emitter_count_df)
 
-        # Visualize the training evolution and final repertoire
-        x = np.arange(num_iterations + 1)
-        zeros = np.zeros(num_iterations + 1)
-        ys = np.row_stack([zeros, emitter_counts]) 
-        y_stack = np.cumsum(ys, axis=0)
+        # For each replication
+        for rep, _ in emitter_counts_df_list[0].iteritems():
+            emitter_counts = []
 
-        fig = plt.figure()
-        ax1 = fig.add_subplot(111)
-        
-        for emitter_num in range(len(emitter_names[exp_num])):
-            ax1.fill_between(x, y_stack[emitter_num, :], y_stack[emitter_num+1,:], alpha=0.7, label=emitter_labels[exp_num][emitter_num])
-            ax1.legend()
-        
-        plt.title(f"Emitter Counts for {exp_name} Experiment")
-        plt.savefig(os.path.join(save_dir, f"{exp_name}_emitter_counts"))
-        plt.close()
+            # get moving average counts of each emitter (for this replication)
+            for emitter_num in range(len(emitter_names[exp_name])):
+                emitter_count = emitter_counts_df_list[emitter_num][rep]
+                average_emitter_count = moving_average(emitter_count, rolling_window_size)
+                emitter_counts.append(average_emitter_count)
+
+            # Visualize the cumulative emitter counts for this experiment and replication
+            x_range = num_iterations - rolling_window_size + 1
+            x = np.arange(x_range + 1)
+            zeros = np.zeros(x_range + 1)
+            ys = np.row_stack([zeros, emitter_counts]) 
+            y_stack = np.cumsum(ys, axis=0)
+
+            fig = plt.figure()
+            ax1 = fig.add_subplot(111)
+            
+            for emitter_num in range(len(emitter_names[exp_name])):
+                ax1.fill_between(x, y_stack[emitter_num, :], y_stack[emitter_num+1,:], alpha=0.7, label=emitter_labels[exp_name][emitter_num])
+                ax1.legend()
+            
+            plt.title(f"Emitter Counts for {experiment_labels[exp_num]} Experiment")
+            plt.savefig(os.path.join(save_dir, f"{exp_name}_emitter_counts_rep_{rep}"))
+            plt.close()
 
 
 def plot_coverage_scores(median_metrics: List[pd.DataFrame],
     lq_metrics: List[pd.DataFrame],
     uq_metrics: List[pd.DataFrame],
-    experiment_names: List[str],
+    experiment_labels: List[str],
     save_dir: str,
 ) -> None:
 
@@ -249,7 +277,7 @@ def plot_coverage_scores(median_metrics: List[pd.DataFrame],
     fig = plt.figure()
     ax1 = fig.add_subplot(111)  
 
-    for exp_num, exp_name in enumerate(experiment_names):
+    for exp_num, exp_name in enumerate(experiment_labels):
         ax1.set_xlabel(x_label)
         ax1.set_ylabel("Coverage")
         ax1.set_title("Coverage evolution during training")
@@ -345,13 +373,24 @@ def get_configs(output_path) -> List[Dict]:
 
 
 if __name__ == '__main__':
-    dirname = "results/ro_2022-12-08_190941_8ac357a86804a751f1c86757b0c79b4fc46f4613/results/walker2d_multi/"
-    experiment_names = ["brax_mome", "brax_mopga"]
+    dirname = "results/ro_2023-01-05_093551_31c3158b10ec634f8babbaf1211654f34af4eae1/halfcheetah_multi/"
+    experiment_names = ["brax_mome", "brax_mopga_normal", "brax_mopga_only_forward", "brax_mopga_only_energy"]
+    experiment_labels = ["MOME", "MOPGA", "MOPGA (Only Forward Emitter)", "MOPGA (Only Energy Emitter)"]
 
-    emitter_names = [["emitter_mutation_count:", "emitter_variation_count:"], ["emitter_1_count:", "emitter_2_count:", "emitter_3_count:"]]
-    emitter_labels = [["Mutation Emitter", "Variation Emitter"], ["Forward Reward Emitter", "Energy Cost Emitter", "GA Emitter"]]
+    emitter_names = {"brax_mome": ["emitter_mutation_count:", "emitter_variation_count:"], 
+            "brax_mopga_normal": ["emitter_1_count:", "emitter_2_count:", "emitter_3_count:"],
+            "brax_mopga_only_forward": ["emitter_1_count:", "emitter_2_count:"],
+            "brax_mopga_only_energy": ["emitter_1_count:", "emitter_2_count:"]
+    }
+
+    emitter_labels = {"brax_mome": ["Mutation Emitter", "Variation Emitter"], 
+                "brax_mopga_normal": ["Forward Reward Emitter", "Energy Cost Emitter", "GA Emitter"],
+                "brax_mopga_only_forward": ["Forward Reward Emitter", "GA Emitter"],
+                "brax_mopga_only_energy": ["Forward Reward Emitter",  "GA Emitter"],
+    }
     
     run_analysis(dirname, 
-        experiment_names, 
+        experiment_names,
+        experiment_labels, 
         emitter_names,
         emitter_labels)
