ef38320b0162b501c035059ce8c3806960dcef97
hnj21 <hnj21@ic.ac.uk>
2023-01-16 16:15:25 +0000

    add max_rewards to brax_spea2 for normalisation of rewards metrics

--- brax_spea2.py
+++ brax_spea2.py
@@ -112,7 +112,8 @@ def main(config: ExperimentConfig) -> None:
     # Define a metrics function
     moqd_metrics_function = partial(
         default_moqd_metrics,
-        reference_point=jnp.array(reference_point)
+        reference_point=jnp.array(reference_point),
+        max_rewards=jnp.array(max_rewards)
     )
 
     # Prepare the scoring function
