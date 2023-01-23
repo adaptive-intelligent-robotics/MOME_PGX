5563bdbe99efae0e0665f7e49da14d40b810819f
hnj21 <hnj21@ic.ac.uk>
2023-01-23 17:59:44 +0000

    add code for analysing pairwise pf coverage

---
+++ pairwise_coverage_analysis.py
@@ -0,0 +1,147 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import jax
+import jax.numpy as jnp
+import os
+import pandas as pd
+import random
+import seaborn as sns
+from functools import partial
+
+
+
+from typing import List, Dict, Tuple
+from qdax.core.containers.mome_repertoire import MOMERepertoire
+from qdax.utils.pareto_front import compute_pareto_dominance, compute_masked_pareto_front
+
+from qdax.utils.plotting import ( 
+    plot_2d_map_elites_repertoire, 
+    plot_mome_max_scores_evolution,
+    plot_mome_pareto_fronts, 
+    plot_mome_scores_evolution,
+)
+
+
+def analyse_pairwise_coverage(parent_dirname: str,
+    env_names: List[str],
+    experiment_names: List[str],
+) -> None:
+
+    print("CALCULATING PAIRWISE COVERAGE SCORES FOR EACH ENV")
+    print("Table scores: proportion of points in row global pf dominated by col global pf")
+
+    # Calculate coverage scores for each environment
+    for env in env_names:
+
+        print("------------------------------")
+        print(f"          ENV: {env}             ")
+        print("------------------------------")
+        print("\n")
+
+        env_dirname = os.path.join(parent_dirname, f"{env}/")
+
+        _analysis_dir = os.path.join(env_dirname, "analysis/")
+
+        # Calculate coverage scores for each experiment
+        env_pareto_fronts = []
+
+        for experiment in experiment_names:
+            #print(f"------- Finding global PF for: {experiment} --------")
+            global_pareto_fronts = get_pfs(env, env_dirname, experiment)
+            env_pareto_fronts.append(global_pareto_fronts)
+        
+        median_coverages = calculate_pairwise_coverage(env_pareto_fronts, experiment_names)
+        median_coverages = median_coverages.replace(0, np.NaN)
+        median_coverages['mean'] = median_coverages.mean(axis = 1, skipna = True)
+        median_coverages.loc['mean'] = median_coverages.mean(axis = 0, skipna = True)
+
+        median_coverages.to_csv(f"{_analysis_dir}/median_pairwise_coverages.csv")
+        
+        print(median_coverages)
+
+
+def get_pfs(
+    env_name: str,
+    dirname: str, 
+    experiment_name: str
+)-> pd.DataFrame:
+
+    global_pareto_fronts = []
+
+    for experiment_replication in os.scandir(os.path.join(dirname, experiment_name)):
+        fitnesses = jnp.load(os.path.join(experiment_replication, "final/repertoire/fitnesses.npy"))
+        global_pareto_front = get_global_pareto_front(fitnesses)
+        global_pareto_fronts.append(global_pareto_front)
+    return global_pareto_fronts
+
+
+def calculate_pairwise_coverage(
+    global_pareto_fronts: List[jnp.ndarray],
+    experiment_names: List[str]
+):
+    num_replications = len(global_pareto_fronts[0])
+
+    metrics = []
+    for replication in range(num_replications):
+        #print(f"----  CALCULATING PAIRWISE COVERAGE FOR REPLICATION: {replication}  -----")
+        rep_global_pfs = []
+        for exp_num in range(len(experiment_names)):
+            exp_rep_global_pf = global_pareto_fronts[exp_num][replication]
+            rep_global_pfs.append(exp_rep_global_pf)
+        rep_df = calculate_rep_coverage(rep_global_pfs, experiment_names)
+        metrics.append(rep_df)
+
+    metrics_concat = pd.concat(metrics)
+    metrics = metrics_concat.groupby(metrics_concat.index)
+    median_metrics = metrics.median()
+    return median_metrics
+
+
+def calculate_rep_coverage(
+    rep_global_pfs: List[jnp.array],
+    experiment_names: List[str],
+)-> pd.DataFrame:
+    ## Table scores: proportion of points in row global pf dominated by col global pf
+    rep_df = pd.DataFrame(columns=experiment_names, index=experiment_names)
+    for exp1_num, exp1 in enumerate(experiment_names):
+        exp1_dominated_list = []
+        for exp2_num, exp2 in enumerate(experiment_names):
+            partial_dominance_fn = partial(compute_pareto_dominance, batch_of_criteria=rep_global_pfs[exp2_num])
+            exp1_dominated = jax.vmap(partial_dominance_fn)(rep_global_pfs[exp1_num])
+            exp1_dominated_proportion = sum(exp1_dominated)/len(exp1_dominated)
+            exp1_dominated_list.append(exp1_dominated_proportion)
+            #print(f"{exp2} dominates {exp1_dominated_proportion} of {exp1}")
+        rep_df.loc[exp1] = exp1_dominated_list
+    
+    return rep_df
+    
+
+def get_global_pareto_front(
+    fitnesses: jnp.array
+):
+
+    fitnesses = jnp.concatenate(fitnesses, axis=0)
+    mask = jnp.any(fitnesses == -jnp.inf, axis=-1)
+    pareto_mask = compute_masked_pareto_front(fitnesses, mask)
+    pareto_front =  fitnesses[pareto_mask]
+    return pareto_front
+
+
+if __name__ == '__main__':
+    parent_dirname = "results/paper_results/"
+
+    experiment_names = [
+        "mome",
+        "mopga",
+        "nsga2",
+        #"pga",
+        "spea2",
+    ]
+
+    env_names=["walker2d_multi",
+    ]
+
+    analyse_pairwise_coverage(parent_dirname, 
+        env_names,
+        experiment_names,
+    )
